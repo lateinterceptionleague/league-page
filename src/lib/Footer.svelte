20e5c80156632828226998d397578f741ac46af6
lateinterceptionleague <110944842+lateinterceptionleague@users.noreply.github.com>
2022-08-10 22:27:00 -0400

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -85,46 +85,5 @@
 	}
 </style>
 
-<div class="footerSpacer" style="height: {outOfDate ? footerHeightNeedUpdate : footerHeight}px;" />
 
-<!-- footer with update notice -->
-<footer class="{outOfDate ? '' : 'invisible'}" bind:this={elNeedUpdate}>
-	<p class="updateNotice">There is an update available for your League Page. <a href="https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#iv-updates">Follow the Update Instructions</a> to get all of the newest features!</p>
-	<div id="navigation">
-		<ul>
-			{#each tabs as tab}
-				{#if !tab.nest}
-					<li><div class="navLink" on:click={() => goto(tab.dest)}>{tab.label}</div></li>
-				{:else}
-					{#each tab.children as child}
-						<li><div class="navLink" on:click={() => goto(child.dest)}>{child.label}</div></li>
-					{/each}
-				{/if}
-			{/each}
-		</ul>
-	</div>
-	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
-	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
-	<br />
-	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
-	<!-- You can remove the donation link (although any donations to help
-	 maintain and enhance League Page would be greatly appreciated!) -->
-	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
-</footer>
-
-<footer class="{!outOfDate ? '' : 'invisible'}" bind:this={el}>
-	<div id="navigation">
-		<ul>
-			{#each tabs as tab}
-				{#if !tab.nest}
-					<li><div class="navLink" on:click={() => goto(tab.dest)}>{tab.label}</div></li>
-				{:else}
-					{#each tab.children as child}
-						<li><div class="navLink" on:click={() => goto(child.dest)}>{child.label}</div></li>
-					{/each}
-				{/if}
-			{/each}
-		</ul>
-	</div>
 </footer>
