101cc056fc123d7e020abcef15345b97b11eaa7b
lateinterceptionleague <110944842+lateinterceptionleague@users.noreply.github.com>
2022-08-11 00:04:48 -0400

    Update index.svelte

--- src/routes/constitution/index.svelte
+++ src/routes/constitution/index.svelte
@@ -100,7 +100,7 @@
 </style>
 
 <div class="constitution">
-    <h1 class="noUnderscore">LEGENDS LEAGUE CONSTITUTION</h1>
+    <h1 class="noUnderscore">LATE INTERCEPTION LEAGUE CONSTITUTION</h1>
     
     <h2 class="noUnderscore">TABLE OF CONTENTS</h2>
     
