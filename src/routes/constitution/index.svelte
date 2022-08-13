7bfeff04a20da8fb9b3e79a4747556c90cacfa38
lateinterceptionleague <110944842+lateinterceptionleague@users.noreply.github.com>
2022-08-13 19:15:44 -0400

    Update index.svelte

--- src/routes/constitution/index.svelte
+++ src/routes/constitution/index.svelte
@@ -283,7 +283,7 @@
     <h2 class="sectionHeading" bind:this={seven}>Section 7 League Finances</h2>
     
     <h3 bind:this={sevenOne}>7.1 League Dues</h3>
-    <p>League dues are set at $50. Dues are collected through <a href="https://leaguesafe.com/join/4052658">LeagueSafe</a> and collection will be sent out no later than August 26, 2022. All managers must be fully paid up before the start of regular season. In the event that a manager fails to pay before the deadline, a $10 penalty will be addd to their payment and will go towards the payouts at the end of the year.</p>
+    <p>League dues are set at $50 and due by August 26, 2022. Dues are collected through <a href="https://leaguesafe.com/join/4052658">LeagueSafe</a> and collection will be sent out no later than February, 12, 2022. All managers must be fully paid up before the start of regular season. In the event that a manager fails to pay before the deadline, a $10 penalty will be addd to their payment and will go towards the payouts at the end of the year.</p>
     
     <h3 bind:this={sevenTwo}>7.2 Payout</h3>
     <p>League payout is structured as follows:</p>
